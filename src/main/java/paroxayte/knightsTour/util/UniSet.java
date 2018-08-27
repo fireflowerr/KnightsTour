f6da025538c0d72352381db36085ed707a19c974
paroxayte <tristonchav@gmail.com>
2018-08-27 15:32:30 -0500

    derp fix

---
+++ src/main/java/paroxayte/knightsTour/util/UniSet.java
@@ -0,0 +1,25 @@
+package paroxayte.knightsTour.util;
+
+import java.util.LinkedHashSet;
+
+/**
+ * Modified hashset to provide equality under .equals(...) as opposed to
+ * referential equality.
+ */
+@SuppressWarnings("serial")
+public class UniSet<E> extends LinkedHashSet<E> {
+
+  @Override
+  public boolean contains(Object o) {
+    return stream().anyMatch((p) -> p.equals(o));
+  }
+
+  @Override
+  public boolean add(E e) {
+    if (contains(e)) {
+      return false;
+    } else {
+      return super.add(e);
+    }
+  }
+}
